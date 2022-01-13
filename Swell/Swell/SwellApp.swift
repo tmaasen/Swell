5215381a9e42140890a6ea24ba9ef46a413f0842
Tanner Maasen <tmaasen@ITMAC18024.local>
2022-01-12 20:37:10 -0600

    init commit

---
+++ Swell/Swell/SwellApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwellApp.swift
+//  Swell
+//
+//  Created by Tanner Maasen on 1/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
